e6ce5e7ddb6d0772e0ba9950684b59290d8d6458
Veritate <veritate@pop-os.localdomain>
2023-03-31 12:15:02 +0300

    feat: implemented a matrices generation and its multiplication step 5

--- starter-project-python/spaceship/routers/api.py
+++ starter-project-python/spaceship/routers/api.py
@@ -1,8 +1,24 @@
 from fastapi import APIRouter
+import numpy
+
 
 router = APIRouter()
 
 
 @router.get('')
 def hello_world() -> dict:
-    return {'msg': 'Hello, World!'}
+   return {'msg': 'Hello, World!'}
+
+
+@router.get('/matrix')
+def multiply_matrixes():
+   matrix_a = numpy.random.rand(10, 10).tolist()
+   matrix_b = numpy.random.rand(10, 10).tolist()
+   product = numpy.dot(matrix_a, matrix_b).tolist()
+
+
+   return {
+       "matrix_a": matrix_a,
+       "matrix_b": matrix_b,
+       "product": product,
+   }
