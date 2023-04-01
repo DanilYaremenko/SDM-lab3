85260fe77107fa040d5fca5d5b2e2942ae355d86
DanilYaremenko <danilyaremenko@gmail.com>
2023-04-02 02:10:45 +0300

    feat: add starter nodejs project

---
+++ starter-project-nodejs/README.md
@@ -0,0 +1,3 @@
+## Console application with chalk library
+
+This console application shows the most popular words of Ronnie Coleman and his name ASCII format.
---
+++ starter-project-nodejs/main.mjs
@@ -0,0 +1,19 @@
+import chalk from 'chalk';
+
+console.log(chalk.green('Yeah buddy!'));
+console.log(chalk.yellow('Light weight, baby!'));
+console.log(chalk.yellow.bold(`            
+_|_|_|                                  _|                
+_|    _|    _|_|    _|_|_|    _|_|_|          _|_|        
+_|_|_|    _|    _|  _|    _|  _|    _|  _|  _|_|_|_|      
+_|    _|  _|    _|  _|    _|  _|    _|  _|  _|            
+_|    _|    _|_|    _|    _|  _|    _|  _|    _|_|_|                                                                                                                                                            
+`));
+
+console.log(chalk.redBright.bold(`
+_|_|_|              _|                                                
+_|          _|_|    _|    _|_|    _|_|_|  _|_|      _|_|_|  _|_|_|    
+_|        _|    _|  _|  _|_|_|_|  _|    _|    _|  _|    _|  _|    _|  
+_|        _|    _|  _|  _|        _|    _|    _|  _|    _|  _|    _|  
+  _|_|_|    _|_|    _|    _|_|_|  _|    _|    _|    _|_|_|  _|    _|  
+`));
